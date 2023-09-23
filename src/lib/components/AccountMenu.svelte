2ea46e267486b6186e1684c85d27cf0d2d35b7fa
jjmeijer <18067438+JJMeijer@users.noreply.github.com>
2023-09-23 19:40:59 +0200

    login page

--- src/lib/components/AccountMenu.svelte
+++ src/lib/components/AccountMenu.svelte
@@ -32,12 +32,12 @@
 <button on:click={onMenuClick} bind:this={element} class="relative rounded-full h-8 w-8 {open ? "bg-gray-700" : "bg-gray-800"} font-mono flex items-center justify-center cursor-pointer border border-gray-700 hover:bg-gray-700">
     <span>{initial}</span>
     {#if open}
-        <div class="absolute w-40 border border-gray-800/50 flex flex-col right-0 top-full bg-gray-900 rounded-md mt-2 cursor-default">
+        <div class="absolute z-10 w-40 border border-gray-800/50 flex flex-col right-0 top-full bg-gray-900 rounded-md mt-2 cursor-default">
             <span class="text-sm w-full text-center border-b border-gray-800/50 py-2 text-gray-500">
                 {username}
             </span>
             <div class="flex flex-col gap-1">
-                <a href="/logout" class="text-sm text-gray-500 hover:text-gray-400 py-2 text-right px-3">Logout</a>
+                <a data-sveltekit-preload-data="off" href="/logout" class="text-sm text-gray-500 hover:text-gray-400 py-2 text-right px-3">Logout</a>
             </div>
         </div>
     {/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
-	import { Button } from "$lib/components";
+    import { Button } from "$lib/components";
 </script>
 
-<Button label="Login" href="/login" />
+<div class="flex flex-col gap-6 border-2 bg-gray-900/50 border-primary/30 rounded-xl p-8">
+    <p class="text-lg font-bold">YASC - Yet Another Spotify Client.</p>
+    <Button label="Authenticate" href="/login" />
+</div>
